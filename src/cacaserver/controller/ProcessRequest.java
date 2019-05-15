4b998a2e7f38ebd904fe8d4136fb27131a67c080
i-saw-sparks <andresmolinaceti@gmail.com>
2019-05-15 18:54:41 -0500

    Modify de alias

--- src/cacaserver/controller/ProcessRequest.java
+++ src/cacaserver/controller/ProcessRequest.java
@@ -13,6 +13,7 @@ import cacaserver.requests.FriendRequest;
 import cacaserver.requests.GetConversation;
 import cacaserver.requests.GetGroupConversation;
 import cacaserver.requests.Login;
+import cacaserver.requests.ModifyAlias;
 import cacaserver.requests.ModifyGroup;
 import cacaserver.requests.NewGroup;
 import cacaserver.requests.NewPersonalMssg;
@@ -27,69 +28,71 @@ import java.util.logging.Logger;
  *
  * @author ivan_
  */
-public class ProcessRequest 
-{
+public class ProcessRequest {
+
     private static JsonParser parser;
     private static Logger logger;
-    
-    static 
-    {
+
+    static {
         parser = new JsonParser();
         logger = Logger.getLogger("ProcessRequest");
     }
-    
-    /***
+
+    /**
+     * *
      * Constructor de request
+     *
      * @param request String del JsonObject inicial
      * @param sender Quien envió el mensaje
-     * @param context Por si necesitan enviar respuestas, broadcasts, etc...
-     * Es un fácil acceso al Server fuera del server. 
+     * @param context Por si necesitan enviar respuestas, broadcasts, etc... Es
+     * un fácil acceso al Server fuera del server.
      */
-    public static void processRequest(String request, Socket sender, Context context) 
-    {
-        logger.info("Got a package from "+sender.getInetAddress());
+    public static void processRequest(String request, Socket sender, Context context) {
+        logger.info("Got a package from " + sender.getInetAddress());
         JsonObject response = parser.parse(request).getAsJsonObject();
         JsonObject args = response.get("args").getAsJsonObject();
-        switch(response.get("type").getAsString())
-        {
+        switch (response.get("type").getAsString()) {
             case "login":
-                new Login(args,sender, context); 
+                new Login(args, sender, context);
                 break;
             case "sign":
-                new Sign(args,sender);
+                new Sign(args, sender);
                 break;
             case "friend-request":
-                new FriendRequest(args,sender, context);
+                new FriendRequest(args, sender, context);
                 break;
             case "accept-request": //Aceptar amigo 
-                new AcceptRequest(args,sender, context);
+                new AcceptRequest(args, sender, context);
                 break;
             case "newGroup": //Nuevo grupo 
-                new NewGroup(args, sender,context);
+                new NewGroup(args, sender, context);
                 break;
             case "add-to-group":  //Agregar al grupo
                 new AddToGroup(args, sender, context);
                 break;
             case "accept-group-request": //Aceptar solicitud de grupo
-                new AcceptGroupRequest(args,sender,context);
+                new AcceptGroupRequest(args, sender, context);
                 break;
             case "refresh": //Refrescar 
-                new Refresh(args,sender,context);
+                new Refresh(args, sender, context);
                 break;
             case "deleteGroup": //Borrar grupo
-                new DeleteGroup (args, sender, context);
+                new DeleteGroup(args, sender, context);
                 break;
             case "modifyGroup": //Cambiar asunto
-                new ModifyGroup (args, sender, context);
+                new ModifyGroup(args, sender, context);
                 break;
             case "newPersonal": //Mensaje personal individual
-                new NewPersonalMssg (args, sender, context);
+                new NewPersonalMssg(args, sender, context);
                 break;
             case "getPersonal": //Obtener mensajes chat normal 
-                new GetConversation(args,sender, context);
+                new GetConversation(args, sender, context);
                 break;
             case "getGroup": //Obtener mensajes chat grupal
-                new GetGroupConversation(args,sender,context);
+                new GetGroupConversation(args, sender, context);
+                break;
+            case "modifyAlias":
+                new ModifyAlias(args, sender, context);
                 break;
             default:
                 break;
